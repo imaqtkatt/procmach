ba9599b5d07ee6733ec525005b08a3723baa416d
imaqtkatt <135721694+imaqtkatt@users.noreply.github.com>
2025-06-10 13:43:56 -0300

    implement exit reason

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::{
   sync::atomic::AtomicUsize,
 };
 
+mod tests;
+
 static PID: AtomicUsize = AtomicUsize::new(0);
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -29,6 +31,7 @@ impl Pid {
 pub struct Atom(u32);
 
 const ATOM_EXIT: Atom = Atom(0);
+const ATOM_NORMAL: Atom = Atom(1);
 
 #[derive(Clone, Copy, Debug)]
 pub struct HeapHandle {
@@ -79,11 +82,14 @@ pub enum Instruction {
   Store(u16),
   Load(u16),
   Goto(u16),
+  Return,
+  ExitReason,
   Exit,
 }
 
 #[derive(Clone, Debug)]
 pub struct Frame {
+  pub ip: Cell<usize>,
   pub code: Vec<Instruction>,
   pub locals: Vec<Value>,
 }
@@ -151,6 +157,8 @@ pub struct Process {
   pub links: Vec<Pid>,
   pub monitors: Vec<Pid>,
   pub heap: Heap,
+  /// if None, exit normally
+  pub exit_reason: Option<Value>,
 }
 
 impl Process {
@@ -169,6 +177,15 @@ impl Process {
   }
 
   pub fn step(&mut self) -> ProcessState {
+    macro_rules! pop_or_exit {
+      ($self:expr, $name:ident) => {
+        let Some($name) = $self.stack.pop() else {
+          $self.state = ProcessState::Exiting;
+          return $self.state;
+        };
+      };
+    }
+
     let instruction = self.fetch();
     println!("{:?} : step {instruction:?}", self.pid);
     match instruction {
@@ -176,7 +193,7 @@ impl Process {
         println!("{:?}", self.stack);
       }
       Instruction::DbgDrop => {
-        let value = self.stack.pop().expect("not empty");
+        pop_or_exit!(self, value);
         fn debug_value(
           value: &Value,
           heap: &Heap,
@@ -190,11 +207,11 @@ impl Process {
               HeapValue::Str(str) => write!(f, "{str}"),
               HeapValue::Tuple(values) => {
                 write!(f, "{{")?;
-                for i in 0..values.len() {
+                for (i, value) in values.iter().enumerate() {
                   if i != 0 {
                     write!(f, ", ")?;
                   }
-                  debug_value(&values[i], heap, f)?;
+                  debug_value(value, heap, f)?;
                 }
                 write!(f, "}}")
               }
@@ -218,14 +235,14 @@ impl Process {
       }
       Instruction::GetTuple(len) => match self.stack.pop() {
         Some(Value::Heap(handle)) => match self.heap.get(handle) {
-          HeapValue::Tuple(values) if (len as usize) < values.len() as usize => {
+          HeapValue::Tuple(values) if (len as usize) < values.len() => {
             let value = values[len as usize];
             self.stack.push(value);
           }
           HeapValue::Tuple(_) => panic!("arity error"),
           HeapValue::Str(_) => panic!("expected tuple"),
         },
-        None | _ => panic!("expected value"),
+        _ => panic!("expected value"),
       },
       Instruction::Add | Instruction::Sub | Instruction::Mul | Instruction::Div => {
         let Value::Int(rhs) = self.stack.pop().expect("rhs") else {
@@ -275,7 +292,7 @@ impl Process {
         }
       }
       Instruction::Send => {
-        let message = self.stack.pop().expect("message");
+        pop_or_exit!(self, message);
         let Value::Pid(pid) = self.stack.pop().unwrap() else {
           panic!("expected pid")
         };
@@ -285,14 +302,13 @@ impl Process {
             .processes
             .get_mut(&pid)
             .expect("process exists");
-          let other_pid = other.pid.clone();
 
           let message = self.heap.deep_clone_at(message, &mut other.heap);
           other.mailbox.push_back(message);
 
           if other.state == ProcessState::Waiting {
             other.state = ProcessState::Runnable;
-            (*scheduler).task_queue.push_back(other_pid);
+            (*scheduler).task_queue.push_back(other.pid);
           }
         }
       }
@@ -305,16 +321,26 @@ impl Process {
       },
       Instruction::Store(index) => {
         let frame = self.frames.last_mut().expect("not empty");
-        let value = self.stack.pop().expect("value");
+        pop_or_exit!(self, value);
         frame.locals[index as usize] = value;
       }
       Instruction::Load(index) => {
         let frame = self.frames.last_mut().expect("not empty");
-        let value = frame.locals[index as usize].clone();
+        let value = frame.locals[index as usize];
         self.stack.push(value);
       }
       Instruction::Goto(index) => {
         self.ip.set(index as usize);
+      }
+      Instruction::Return => match self.frames.pop() {
+        Some(frame) => {
+          self.ip = frame.ip;
+        }
+        None => self.state = ProcessState::Exiting,
+      },
+      Instruction::ExitReason => {
+        self.exit_reason = Some(self.stack.pop().unwrap());
+        self.state = ProcessState::Exiting;
       }
       Instruction::Exit => self.state = ProcessState::Exiting,
     };
@@ -342,17 +368,11 @@ impl Scheduler {
   pub fn run(&mut self) {
     while let Some(pid) = self.task_queue.pop_front() {
       let process = match self.processes.get_mut(&pid) {
-        Some(process) => {
-          println!("{pid:?} : queued");
-          process
-        }
-        None => {
-          eprintln!("{pid:?} : ignored");
-          continue;
-        }
+        Some(process) => process,
+        None => panic!("dangling process : {pid:?}"),
       };
 
-      let mut w = 0u8;
+      let mut w = 0i32;
 
       while (w < 8) & (process.state != ProcessState::Exiting) {
         match process.step() {
@@ -372,25 +392,48 @@ impl Scheduler {
         ProcessState::Exiting => {
           let process = self.processes.remove(&pid).unwrap();
 
-          for linked_pid in process.links {
-            // TODO: cascading?
-            if let Some(link) = self.processes.get_mut(&linked_pid) {
-              eprintln!(
-                "process {:?} exiting due to linked {:?}",
-                link.pid, process.pid
-              );
-              if link.state != ProcessState::Exiting {
-                link.state = ProcessState::Exiting;
-                self.task_queue.push_front(linked_pid);
+          if process.exit_reason.is_some() {
+            let exit_reason = process.exit_reason.unwrap();
+
+            for linked_pid in &process.links {
+              // TODO: cascading?
+              if let Some(link) = self.processes.get_mut(&linked_pid) {
+                eprintln!(
+                  "process {:?} exiting due to linked {:?}",
+                  link.pid, process.pid
+                );
+
+                // TODO: trap exit
+                link.exit_reason = Some(process.heap.deep_clone_at(exit_reason, &mut link.heap));
+
+                if link.state != ProcessState::Exiting {
+                  link.state = ProcessState::Exiting;
+                  self.task_queue.push_front(*linked_pid);
+                }
               }
             }
           }
 
+          let mut process = process;
+
           for monitoring_pid in process.monitors {
             if let Some(monitor) = self.processes.get_mut(&monitoring_pid) {
-              let exit = monitor
-                .heap
-                .allocate_tuple(vec![Value::Atom(ATOM_EXIT), Value::Pid(pid)]);
+              let exit = match process.exit_reason {
+                Some(reason) => {
+                  let exit = process.heap.allocate_tuple(vec![
+                    Value::Atom(ATOM_EXIT),
+                    Value::Pid(pid),
+                    reason,
+                  ]);
+                  process.heap.deep_clone_at(exit, &mut monitor.heap)
+                }
+                None => monitor.heap.allocate_tuple(vec![
+                  Value::Atom(ATOM_EXIT),
+                  Value::Pid(pid),
+                  Value::Atom(ATOM_NORMAL),
+                ]),
+              };
+
               monitor.mailbox.push_back(exit);
 
               if monitor.state == ProcessState::Waiting {
@@ -407,210 +450,5 @@ impl Scheduler {
 }
 
 fn main() {
-  let process_exit = {
-    let this_frame = Frame {
-      code: vec![
-        Instruction::PushInt(0),
-        Instruction::PushInt(3),
-        Instruction::Sub,
-        Instruction::Exit,
-      ],
-      locals: vec![],
-    };
-    Process {
-      pid: Pid::next(),
-      ip: Cell::new(0),
-      state: ProcessState::Runnable,
-      stack: vec![],
-      frames: vec![this_frame],
-      mailbox: VecDeque::new(),
-      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
-      links: vec![],
-      monitors: vec![],
-      heap: Heap::default(),
-    }
-  };
-
-  let process_main = {
-    let this_frame = Frame {
-      code: vec![
-        Instruction::PushPid(process_exit.pid),
-        Instruction::Monitor,
-        Instruction::Receive,
-        Instruction::DbgDrop,
-        Instruction::Goto(3),
-        Instruction::Exit,
-      ],
-      locals: vec![],
-    };
-    Process {
-      pid: Pid::next(),
-      ip: Cell::new(0),
-      state: ProcessState::Runnable,
-      stack: vec![],
-      frames: vec![this_frame],
-      mailbox: VecDeque::new(),
-      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
-      links: vec![],
-      monitors: vec![],
-      heap: Heap::default(),
-    }
-  };
-
-  let scheduler = GLOBAL_SCHEDULER.with(Rc::clone);
-
-  unsafe {
-    let scheduler = scheduler.get();
-    (*scheduler).add_process(process_main);
-    (*scheduler).add_process(process_exit);
-    (*scheduler).run();
-  }
-}
-
-#[allow(dead_code)]
-fn program_adder() {
-  let process_main = {
-    let main_frame = Frame {
-      code: vec![
-        Instruction::Receive,
-        Instruction::Store(0),
-        Instruction::Receive,
-        Instruction::Store(1),
-        Instruction::Load(0),
-        Instruction::Load(1),
-        Instruction::Add,
-        Instruction::DbgStack,
-        Instruction::Exit,
-      ],
-      locals: vec![Value::Int(69), Value::Int(69)],
-    };
-    Process {
-      pid: Pid::next(),
-      ip: Cell::new(0),
-      state: ProcessState::Runnable,
-      stack: vec![],
-      frames: vec![main_frame],
-      mailbox: VecDeque::new(),
-      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
-      links: vec![],
-      monitors: vec![],
-      heap: Heap::default(),
-    }
-  };
-
-  let process_sender = {
-    let frame = Frame {
-      code: vec![
-        Instruction::PushPid(process_main.pid),
-        Instruction::PushInt(3),
-        Instruction::Send,
-        Instruction::PushPid(process_main.pid),
-        Instruction::PushInt(3),
-        Instruction::Send,
-        Instruction::Exit,
-      ],
-      locals: vec![],
-    };
-    Process {
-      pid: Pid::next(),
-      ip: Cell::new(0),
-      state: ProcessState::Runnable,
-      stack: vec![],
-      frames: vec![frame],
-      mailbox: VecDeque::new(),
-      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
-      links: vec![],
-      monitors: vec![],
-      heap: Heap::default(),
-    }
-  };
-
-  let scheduler = GLOBAL_SCHEDULER.with(Rc::clone);
-
-  unsafe {
-    let scheduler = scheduler.get();
-    (*scheduler).add_process(process_main);
-    (*scheduler).add_process(process_sender);
-    (*scheduler).run();
-  }
-}
-
-#[allow(dead_code)]
-fn program_link() {
-  let process_exit1 = {
-    let this_frame = Frame {
-      code: vec![Instruction::DbgStack, Instruction::Exit],
-      locals: vec![],
-    };
-    Process {
-      pid: Pid::next(),
-      ip: Cell::new(0),
-      state: ProcessState::Runnable,
-      stack: vec![],
-      frames: vec![this_frame],
-      mailbox: VecDeque::new(),
-      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
-      links: vec![],
-      monitors: vec![],
-      heap: Heap::default(),
-    }
-  };
-
-  let process_exit2 = {
-    let this_frame = Frame {
-      code: vec![
-        Instruction::PushPid(process_exit1.pid),
-        Instruction::Link,
-        Instruction::Goto(2),
-        Instruction::Exit,
-      ],
-      locals: vec![],
-    };
-    Process {
-      pid: Pid::next(),
-      ip: Cell::new(0),
-      state: ProcessState::Runnable,
-      stack: vec![],
-      frames: vec![this_frame],
-      mailbox: VecDeque::new(),
-      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
-      links: vec![],
-      monitors: vec![],
-      heap: Heap::default(),
-    }
-  };
-
-  let process_main = {
-    let main_frame = Frame {
-      code: vec![
-        Instruction::PushPid(process_exit2.pid),
-        Instruction::Link,
-        Instruction::Goto(2),
-        Instruction::Exit,
-      ],
-      locals: vec![],
-    };
-    Process {
-      pid: Pid::next(),
-      ip: Cell::new(0),
-      state: ProcessState::Runnable,
-      stack: vec![],
-      frames: vec![main_frame],
-      mailbox: VecDeque::new(),
-      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
-      links: vec![],
-      monitors: vec![],
-      heap: Heap::default(),
-    }
-  };
-
-  let scheduler = GLOBAL_SCHEDULER.with(Rc::clone);
-
-  unsafe {
-    let scheduler = scheduler.get();
-    (*scheduler).add_process(process_main);
-    (*scheduler).add_process(process_exit2);
-    (*scheduler).add_process(process_exit1);
-    (*scheduler).run();
-  }
+  tests::program_monitor();
 }
---
+++ src/tests.rs
@@ -0,0 +1,227 @@
+use std::{cell::Cell, collections::VecDeque, rc::Rc};
+
+use crate::{Frame, GLOBAL_SCHEDULER, Heap, Instruction, Pid, Process, ProcessState, Value};
+
+#[allow(dead_code)]
+pub fn program_monitor() {
+  let process_exit = {
+    let this_frame = Frame {
+      ip: Cell::default(),
+      code: vec![Instruction::PushInt(3), Instruction::Sub, Instruction::Exit],
+      locals: vec![],
+    };
+    let frame_return_zero = Frame {
+      ip: Cell::default(),
+      code: vec![Instruction::PushInt(0), Instruction::Return],
+      locals: vec![],
+    };
+    Process {
+      pid: Pid::next(),
+      ip: Cell::new(0),
+      state: ProcessState::Runnable,
+      stack: vec![],
+      frames: vec![this_frame, frame_return_zero],
+      mailbox: VecDeque::new(),
+      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
+      links: vec![],
+      monitors: vec![],
+      heap: Heap::default(),
+      exit_reason: None,
+    }
+  };
+
+  let process_main = {
+    let this_frame = Frame {
+      ip: Cell::default(),
+      code: vec![
+        Instruction::PushPid(process_exit.pid),
+        Instruction::Monitor,
+        Instruction::Receive,
+        Instruction::DbgDrop,
+        Instruction::Goto(3),
+        Instruction::Exit,
+      ],
+      locals: vec![],
+    };
+    Process {
+      pid: Pid::next(),
+      ip: Cell::new(0),
+      state: ProcessState::Runnable,
+      stack: vec![],
+      frames: vec![this_frame],
+      mailbox: VecDeque::new(),
+      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
+      links: vec![],
+      monitors: vec![],
+      heap: Heap::default(),
+      exit_reason: None,
+    }
+  };
+
+  let scheduler = GLOBAL_SCHEDULER.with(Rc::clone);
+
+  unsafe {
+    let scheduler = scheduler.get();
+    (*scheduler).add_process(process_main);
+    (*scheduler).add_process(process_exit);
+    (*scheduler).run();
+  }
+}
+
+#[allow(dead_code)]
+pub fn program_adder() {
+  let process_main = {
+    let main_frame = Frame {
+      ip: Cell::default(),
+      code: vec![
+        Instruction::Receive,
+        Instruction::Store(0),
+        Instruction::Receive,
+        Instruction::Store(1),
+        Instruction::Load(0),
+        Instruction::Load(1),
+        Instruction::Add,
+        Instruction::DbgStack,
+        Instruction::Exit,
+      ],
+      locals: vec![Value::Int(69), Value::Int(69)],
+    };
+    Process {
+      pid: Pid::next(),
+      ip: Cell::new(0),
+      state: ProcessState::Runnable,
+      stack: vec![],
+      frames: vec![main_frame],
+      mailbox: VecDeque::new(),
+      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
+      links: vec![],
+      monitors: vec![],
+      heap: Heap::default(),
+      exit_reason: None,
+    }
+  };
+
+  let process_sender = {
+    let frame = Frame {
+      ip: Cell::default(),
+      code: vec![
+        Instruction::PushPid(process_main.pid),
+        Instruction::PushInt(3),
+        Instruction::Send,
+        Instruction::PushPid(process_main.pid),
+        Instruction::PushInt(3),
+        Instruction::Send,
+        Instruction::Exit,
+      ],
+      locals: vec![],
+    };
+    Process {
+      pid: Pid::next(),
+      ip: Cell::new(0),
+      state: ProcessState::Runnable,
+      stack: vec![],
+      frames: vec![frame],
+      mailbox: VecDeque::new(),
+      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
+      links: vec![],
+      monitors: vec![],
+      heap: Heap::default(),
+      exit_reason: None,
+    }
+  };
+
+  let scheduler = GLOBAL_SCHEDULER.with(Rc::clone);
+
+  unsafe {
+    let scheduler = scheduler.get();
+    (*scheduler).add_process(process_main);
+    (*scheduler).add_process(process_sender);
+    (*scheduler).run();
+  }
+}
+
+#[allow(dead_code)]
+pub fn program_link() {
+  let process_exit1 = {
+    let this_frame = Frame {
+      ip: Cell::default(),
+      code: vec![Instruction::PushInt(99), Instruction::ExitReason],
+      locals: vec![],
+    };
+    Process {
+      pid: Pid::next(),
+      ip: Cell::new(0),
+      state: ProcessState::Runnable,
+      stack: vec![],
+      frames: vec![this_frame],
+      mailbox: VecDeque::new(),
+      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
+      links: vec![],
+      monitors: vec![],
+      heap: Heap::default(),
+      exit_reason: None,
+    }
+  };
+
+  let process_exit2 = {
+    let this_frame = Frame {
+      ip: Cell::default(),
+      code: vec![
+        Instruction::PushPid(process_exit1.pid),
+        Instruction::Link,
+        Instruction::Goto(2),
+        Instruction::Exit,
+      ],
+      locals: vec![],
+    };
+    Process {
+      pid: Pid::next(),
+      ip: Cell::new(0),
+      state: ProcessState::Runnable,
+      stack: vec![],
+      frames: vec![this_frame],
+      mailbox: VecDeque::new(),
+      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
+      links: vec![],
+      monitors: vec![],
+      heap: Heap::default(),
+      exit_reason: None,
+    }
+  };
+
+  let process_main = {
+    let main_frame = Frame {
+      ip: Cell::default(),
+      code: vec![
+        Instruction::PushPid(process_exit2.pid),
+        Instruction::Link,
+        Instruction::Goto(2),
+        Instruction::Exit,
+      ],
+      locals: vec![],
+    };
+    Process {
+      pid: Pid::next(),
+      ip: Cell::new(0),
+      state: ProcessState::Runnable,
+      stack: vec![],
+      frames: vec![main_frame],
+      mailbox: VecDeque::new(),
+      scheduler: GLOBAL_SCHEDULER.with(Rc::clone),
+      links: vec![],
+      monitors: vec![],
+      heap: Heap::default(),
+      exit_reason: None,
+    }
+  };
+
+  let scheduler = GLOBAL_SCHEDULER.with(Rc::clone);
+
+  unsafe {
+    let scheduler = scheduler.get();
+    (*scheduler).add_process(process_main);
+    (*scheduler).add_process(process_exit2);
+    (*scheduler).add_process(process_exit1);
+    (*scheduler).run();
+  }
+}
